6ef0fb3846058544b746d7af2b203384d5005bc2
Dao Nguyen <daon@us.ibm.com>
2017-05-02 14:25:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,34 +11,36 @@ app.use(bodyParser.json())
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mysqlURL = process.env.OPENSHIFT_MYSQL_DB_URL || process.env.MYSQL_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-if (mysqlURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mysqlServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+var mysqlServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mysqlServiceNameHost = process.env[mysqlServiceName + '_SERVICE_HOST'],
       mysqlServiceNamePort = process.env[mysqlServiceName + '_SERVICE_PORT'],
       mysqlServiceNameDatabase = process.env[mysqlServiceName + '_DATABASE'],
-      mysqlServiceNamePassword = process.env[mysqlServiceName + '_PASSWORD']
+      mysqlServiceNamePassword = process.env[mysqlServiceName + '_PASSWORD'],
       mysqlServiceNameUser = process.env[mysqlServiceName + '_USER'];
-  var connection = mysql.createConnection({
+    console.log("mysqlServiceName=", process.env.DATABASE_SERVICE_NAME);
+    console.log("mysqlServiceNameHost=", mysqlServiceNameHost);
+    console.log("mysqlServiceNameDatabase=", mysqlServiceNameDatabase);
+    console.log("mysqlServiceNamePassword=", mysqlServiceNamePassword);
+    
+ var connection = mysql.createConnection({
     host     : mysqlServiceNameHost,
     user     : mysqlServiceNameUser,
     password : mysqlServiceNamePassword,
     database : mysqlServiceNameDatabase
   });
 
-  connection.connect();
 
-  connection.query('SELECT * from users', function(err, rows, fields) {
-    if (!err)
-      console.log('The solution is: ', rows);
-    else
-      console.log('Error while performing Query.');
+  connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... nn");    
+    } else {
+        console.log("Error connecting database ... nn");    
+    }
   });
+  
 
-  connection.end();
 };
 /******************************
 * APIs 
@@ -46,6 +48,22 @@ if (mysqlURL == null && process.env.DATABASE_SERVICE_NAME) {
 
 app.get('/', function(req, res) {
   res.send("Hello World!!!")
+});
+app.get('/data', function(req, res) {
+    connection.query('SELECT * from users', function(err, rows, fields) {
+    if (!err){
+      console.log('The solution is: ', rows);
+        res.send(rows);
+    }
+    else
+      console.log('Error while performing Query.');
+  });
+  connection.end(function(err){
+    if(!err) {
+        console.log("Database is disconnected ... nn");    
+    } else {
+        console.log("Error disconnecting database ... nn");    
+    });
 });
 
 
