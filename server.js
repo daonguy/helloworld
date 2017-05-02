c8d95724c1fe78c32873eb1e8e9ac2dedbfbb7c9
Dao Nguyen <daon@us.ibm.com>
2017-05-02 13:33:50 -0400

    Add mysql

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser')
+var mysql      = require('mysql');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -8,6 +9,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mysqlURL = process.env.OPENSHIFT_MYSQL_DB_URL || process.env.MYSQL_URL,
+    mongoURLLabel = "";
+
+if (mysqlURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mysqlServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mysqlServiceNameHost = process.env[mysqlServiceName + '_SERVICE_HOST'],
+      mysqlServiceNamePort = process.env[mysqlServiceName + '_SERVICE_PORT'],
+      mysqlServiceNameDatabase = process.env[mysqlServiceName + '_DATABASE'],
+      mysqlServiceNamePassword = process.env[mysqlServiceName + '_PASSWORD']
+      mysqlServiceNameUser = process.env[mysqlServiceName + '_USER'];
+  var connection = mysql.createConnection({
+    host     : mysqlServiceNameHost,
+    user     : mysqlServiceNameUser,
+    password : mysqlServiceNamePassword,
+    database : mysqlServiceNameDatabase
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * from users', function(err, rows, fields) {
+    if (!err)
+      console.log('The solution is: ', rows);
+    else
+      console.log('Error while performing Query.');
+  });
+
+  connection.end();
+};
 /******************************
 * APIs 
 *******************************/
@@ -17,7 +49,7 @@ app.get('/', function(req, res) {
 });
 
 
-var port = process.env.PORT || 9080;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log('App started on port:' + port);
 });
